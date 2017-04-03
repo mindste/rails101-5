f485fdef4f49afcebe40386ff37a3a048ae4bdca
robote <2233628310@qq.com>
2017-04-03 18:15:38 +0800

    check  owner  permission  when  access  edit/update/destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action  :authenticate_user! ,  only: [:new, :create]
+  before_action  :authenticate_user! ,  only: [:new, :create, :edit, :update, :destroy]
 
   def  index
     @movies  =  Movie.all
@@ -16,7 +16,7 @@ class MoviesController < ApplicationController
   def  create
     @movie  =  Movie.new(movie_params)
     @movie.user  =  current_user
-    
+
     if  @movie.save
       redirect_to  movies_path
     else
@@ -26,10 +26,19 @@ class MoviesController < ApplicationController
 
   def  edit
     @movie  =  Movie.find(params[:id])
+
+    if  current_user  !=  @movie.user
+      redirect_to  root_path,  alert:  "You  have  no  permission."
+    end
   end
 
   def  update
     @movie  =  Movie.find(params[:id])
+
+    if  current_user  !=  @movie.user
+      redirect_to  root_path,  alert:  "You  have  no  permission."
+    end
+
     if  @movie.update(movie_params)
       redirect_to  movies_path,  notice: "更新成功！"
     else
@@ -39,6 +48,11 @@ class MoviesController < ApplicationController
 
   def  destroy
     @movie  =  Movie.find(params[:id])
+
+    if  current_user  !=  @movie.user
+      redirect_to  root_path,  alert:  "You  have  no  permission."
+    end
+    
     @movie.destroy
 
   redirect_to  movies_path，alert: "删除成功！"
